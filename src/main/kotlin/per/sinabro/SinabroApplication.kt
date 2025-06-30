b0b9a9db728945e161f8de731c47cc87529bdc53
ddu0422 <ddu0422@naver.com>
2025-06-30 20:00:26 +0900

    chore: init project

---
+++ src/main/kotlin/per/sinabro/SinabroApplication.kt
@@ -0,0 +1,11 @@
+package per.sinabro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SinabroApplication
+
+fun main(args: Array<String>) {
+    runApplication<SinabroApplication>(*args)
+}
